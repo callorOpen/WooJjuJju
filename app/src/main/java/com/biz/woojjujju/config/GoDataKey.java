7de1dbb2ae1db27de1ed998f1ad7231674a2ef4e
wynSOO <tngus4020@naver.com>
2018-09-29 19:27:42 +0900

    0929

--- app/src/main/java/com/biz/woojjujju/config/GoDataKey.java
+++
@@ -1,9 +0,0 @@
-package com.biz.woojjujju.config;
-
-public class GoDataKey {
-
-    public static String GoAPIkey ="UfvJEkgr2P7LfUQ%2Bs7IAHi%2BWj%2B7up5oGGbDrRSAQWeif6Ftr7SYufzh0IQwl19hYBpSUiXgp1Sust%2F31FxUwug%3D%3D";
-    public static String GoAPIurl ="http://www.bokjiro.go.kr/openapi/rest/gvmtWelSvc";
-
-
-}
